3ab5e153915a2e137a18e1d58ef9f561229aa4ec
Saurabh <reachtoskv@gmail.com>
2025-01-26 17:31:53 +0530

    Adding project to github

---
+++ skv2024/src/main/java/com/verma/skv2024/Skv2024Application.java
@@ -0,0 +1,13 @@
+package com.verma.skv2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skv2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skv2024Application.class, args);
+	}
+
+}
